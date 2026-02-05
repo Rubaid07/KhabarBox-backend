fd22aa1e0759154921fad77f70042e3c8b26a920
Rubaid07 <rubaidindu@gmail.com>
2026-02-05 16:26:17 +0600

    setup better auth

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.APP_URL || "http://localhost:3000",
+  credentials: true
+}))
+
+app.use(express.json())
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+export default app;
